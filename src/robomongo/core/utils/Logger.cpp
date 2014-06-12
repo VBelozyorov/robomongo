1388d09980b678376aeaf858aea97e703978eacd
Vladimir Belozyorov <vladimir.belozyorov@gmail.com>
2014-06-12 14:30:17 +0400

    Writing logs at program cache path QStandardPaths requires Qt5 #465

--- src/robomongo/core/utils/Logger.cpp
+++ src/robomongo/core/utils/Logger.cpp
@@ -2,14 +2,32 @@
 
 #include <QDir>
 #include <QMetaType>
+#include <QStandardPaths>
+
 #include "robomongo/core/utils/QtUtils.h"
 
 namespace
 {
+
     std::string getLoggerPath()
     {
-        static std::string path = Robomongo::QtUtils::toStdString(QString("%1/"PROJECT_NAME_LOWERCASE".log").arg(QDir::tempPath()));
-        return path;
+        // @todo Qt4 approach, wasn't tested
+//        static std::string path = Robomongo::QtUtils::toStdString(
+//            QString("%1/"PROJECT_NAME_LOWERCASE".log").arg(QDesktopServices::storageLocation(QDesktopServices::CacheLocation))
+//        );
+
+        QStringList locations = QStandardPaths::standardLocations(QStandardPaths::CacheLocation);
+        if (locations.isEmpty()) {
+            // @todo Should we write logs in location with potentially common access or don't write it at all?
+            return std::string("", 0);
+        }
+        else {
+            // @todo If locations[0] won't be writable for some reason, logs won't be written
+            static std::string path = Robomongo::QtUtils::toStdString(
+                QString("%1/"PROJECT_NAME_LOWERCASE".log").arg(locations[0])
+            );
+            return path;
+        }
     }
 }
 
@@ -22,6 +40,12 @@ namespace Robomongo
         QFile file(QtUtils::toQString(path)); //delete file if it size more than 5mb
         if (file.exists() && file.size() > 5 * 1024 * 1024) {
             file.remove();
+        } 
+        else {
+            QFileInfo fileInfo(file);
+            QString filePath = fileInfo.path();
+            QDir fileDir;
+            fileDir.mkpath(filePath);
         }
         mongo::initLogging(path,true);
     }
